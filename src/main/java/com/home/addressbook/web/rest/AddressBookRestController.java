cb1c924bd803bd95230206fef410be688135b436
habangar <habangar@gmail.com>
2013-09-02 21:42:43 +0100

    RestController for /name-of-oldest-person + unit tests done.

--- src/main/java/com/home/addressbook/web/rest/AddressBookRestController.java
+++ src/main/java/com/home/addressbook/web/rest/AddressBookRestController.java
@@ -1,5 +1,7 @@
 package com.home.addressbook.web.rest;
 
+import java.io.IOException;
+
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -8,6 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.home.addressbook.service.AddressBookItemService;
 import com.home.addressbook.service.ImportAddressBookService;
@@ -30,6 +35,34 @@ public class AddressBookRestController {
 		try {
 			int numberOfMales = addressBookItemService.getNumberOfMales();
 			return new ResponseEntity<String>(String.valueOf(numberOfMales), HttpStatus.OK);
+		} catch (ServiceException ex) {
+			logger.debug("error", ex);
+			return new ResponseEntity<String>(ex.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(value="/populate")
+	public ResponseEntity<String> populate(@RequestParam("file") MultipartFile file) {
+		if (file.isEmpty()) {
+			return new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
+		}
+		try {
+			int importedItems = importAddressBookService.importItems(file.getInputStream());
+			return new ResponseEntity<String>(String.valueOf(importedItems), HttpStatus.OK);
+		} catch (ServiceException ex) {
+			logger.debug("error", ex);
+			return new ResponseEntity<String>(ex.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (IOException ex) {
+			logger.debug("error", ex);
+			return new ResponseEntity<String>(ex.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(value="/name-of-oldest-person")
+	public ResponseEntity<String> getNameOfOldestPerson() {
+		try {
+			String nameOfOldestPerson = addressBookItemService.getNameOfOldestPerson();
+			return new ResponseEntity<String>(nameOfOldestPerson, HttpStatus.OK);
 		} catch (ServiceException ex) {
 			logger.debug("error", ex);
 			return new ResponseEntity<String>(ex.getLocalizedMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
--- src/test/java/com/home/addressbook/web/rest/AddressBookRestControllerTest.java
+++ src/test/java/com/home/addressbook/web/rest/AddressBookRestControllerTest.java
@@ -1,12 +1,19 @@
 package com.home.addressbook.web.rest;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
 
 import com.home.addressbook.service.AddressBookItemService;
 import com.home.addressbook.service.ImportAddressBookService;
@@ -52,4 +59,51 @@ public class AddressBookRestControllerTest {
 		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
 		verify(addressBookItemService, times(1)).getNumberOfMales();
 	}
+
+	@Test
+	public void testPopulate() throws Exception {
+		when(importAddressBookService.importItems(any(InputStream.class))).thenReturn(11);
+		//
+		MockMultipartFile file = new MockMultipartFile("file", "test-content".getBytes());
+		ResponseEntity<String> response = controller.populate(file);
+		//
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals("11", response.getBody());
+		ArgumentCaptor<InputStream> inputArg = ArgumentCaptor.forClass(InputStream.class);
+		verify(importAddressBookService, times(1)).importItems(inputArg.capture());
+		assertNotNull(inputArg.getValue());
+		StringWriter sw = new StringWriter();
+		IOUtils.copy(inputArg.getValue(), sw);
+		assertEquals("test-content", sw.toString());
+	}
+
+	@Test
+	public void testPopulateEmpty() throws Exception {
+		MockMultipartFile file = new MockMultipartFile("file", "".getBytes());
+		ResponseEntity<String> response = controller.populate(file);
+		//
+		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+	}
+
+	@Test
+	public void testGetOldestPerson() throws Exception {
+		when(addressBookItemService.getNameOfOldestPerson()).thenReturn("Sarah Stone");
+		//
+		ResponseEntity<String> response = controller.getNameOfOldestPerson();
+		//
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals("Sarah Stone", response.getBody());
+		verify(addressBookItemService, times(1)).getNameOfOldestPerson();
+	}
+
+	@Test
+	public void testGetOldestPersonError500() throws Exception {
+		when(addressBookItemService.getNameOfOldestPerson()).thenThrow(new ServiceException());
+		//
+		ResponseEntity<String> response = controller.getNameOfOldestPerson();
+		//
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+		verify(addressBookItemService, times(1)).getNameOfOldestPerson();
+	}
+
 }
